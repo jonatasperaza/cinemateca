2dd7a553dbee075eee868086648e1594b8c8455a
mariaeduardanichelle <madunichelle@gmail.com>
2024-11-06 13:57:50 -0300

    feat: responsividade navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,9 +17,100 @@
             <button>Filmografia</button>
             <button>Início</button>
         </div>
+
+        <div class="dropdown">
+            <button class="dropdown-button">Menu</button>
+            <div class="dropdown-content">
+                <button>Biografia</button>
+                <button>Estilo</button>
+                <button>Filmografia</button>
+                <button>Início</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.navbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 48px;
+    background-color: #1C1C1C;
+}
+.titulo_site {
+    display: flex;
+    width: 16.31rem; 
+    height: 5.63rem;
+}
+h1 {
+    color: #D4E7F7;
+    font-family: Arial, sans-serif;
+    font-size: 1.25rem; 
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.6;
+}
 
+button {
+    background-color: #1C1C1C;
+    border: none;
+    color: #D4E7F7;
+    font-family: Arial, sans-serif;
+    font-style: normal;
+    cursor: pointer;
+}
+
+.menu-left,
+.menu-right {
+    display: flex;
+    gap: 2.7rem;
+}
+
+.dropdown {
+    display: none;
+    position: relative;
+}
+.dropdown-button {
+    font-size: 1rem;
+}
+.dropdown-content {
+    display: none;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    background-color: #1C1C1C;
+    padding: 10px;
+    border-radius: 5px;
+    z-index: 1;
+}
+.dropdown-content button {
+    display: block;
+    margin: 5px 0;
+}
+.dropdown-button:hover + .dropdown-content,
+.dropdown-content:hover {
+    display: block;
+}
+
+@media (max-width: 768px) {
+    .menu-left, .menu-right {
+        display: none;
+    }
+    .dropdown {
+        display: block;
+        margin-right: 25px; 
+    }
+    .navbar {
+        padding: 20px;
+        padding-left: 0px;
+    }
+    .titulo_site {
+        height: 3.625rem;
+    }
+    h1 {
+        font-size: 1rem;
+        line-height: 1.2;
+    }
+}
 </style>
